ddf1b6b93e79f7cb0d3be0c5e0945bd9d54d1cf5
keno1215 <kenoconnell73@gmail.com>
2026-03-31 13:16:19 -0400

    new terraform folder

---
+++ terraform_new_folder/state.tf
@@ -0,0 +1,10 @@
+
+terraform { 
+    backend "s3" {
+        bucket = "my-terraform-state"
+        key = "global/s3/terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "s3-tf-table"
+
+    }
+}
